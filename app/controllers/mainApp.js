d0f32c804efa9cd9243a25c43418ccf5e289ae50
Name-less <ph0enix.fiire@gmail.com>
2015-02-16 12:53:13 +0100

    graphical part of login page almost done. graphical part of sign in page almost done

--- app/controllers/mainApp.js
+++ app/controllers/mainApp.js
@@ -1,25 +1,53 @@
 /**
  * Created by julescantegril on 10/02/2015.
  */
-var mainApp = angular.module('mainApp',['ngMaterial']);
+var mainApp = angular.module('mainApp',['ngMaterial','ngRoute','ngMessages']);
+var isConnected = false;
+
+var user = {'login':'','token':0};
+
+mainApp.config(['$routeProvider',function($routeProvider){
+    $routeProvider.when('/signin',{
+        templateUrl:'signin.html',
+        controller:'signinCtrl'
+    }).when('/login',{
+        templateUrl:'login.html',
+        controller:'loginCtrl'
+    });
+}]);
 
 mainApp.controller('mainCtrl',function($scope,$mdSidenav){
 
     $scope.toggleMenu = function(){
         $mdSidenav('left').toggle();
-    }
+        $scope.userConnected = isConnected;
+    };
 
     $scope.nameProject = 'Security Educational Platform';
     $scope.abreviationProject = 'SEP';
 
+    $scope.userConnected = isConnected;
+
+});
+
+mainApp.controller('loginCtrl',function($scope,$location){
     $scope.login = '';
     $scope.pwd = '';
 
-    $scope.userConnected = false;
+    $scope.userConnected = isConnected;
 
     $scope.connect = function(){
-        $scope.userConnected = true;
-        //$scope.boobs = $scope.login + ' dddddd ' +$scope.pwd;
+        isConnected  = true;
+        $scope.userConnected = isConnected;
+        user.login = $scope.login;
+        //$location.path('/signin');
+    };
+
+    $scope.deconnect = function(){
+        $scope.login = '';
+        $scope.pwd = '';
+        $scope.userOrPwdChange();
+        $scope.userConnected = false;
     };
 
     $scope.userAndPassPresent = false;
@@ -31,7 +59,38 @@ mainApp.controller('mainCtrl',function($scope,$mdSidenav){
         }
     };
 
+    $scope.go = function(path){
+        $location.path(path);
+    }
 
+});
 
+mainApp.controller('signinCtrl',function($scope){
+
+    $scope.email = '';
+    $scope.pwd = '';
+    $scope.pwd2 = '';
+
+    $scope.pwdOk = false;
+    $scope.emailOk = false;
+
+    $scope.everyInfoOk = false;
+
+    $scope.checkPwds = function(){
+        if($scope.pwd === $scope.pwd2){
+            $scope.pwdOk = true;
+        }else{
+            $scope.pwdOk = false;
+        }
+    }
+
+    $scope.checkEmail = function(){
+        //ask the server is the email is taken or not
+        $scope.emailOk = true;
+    }
+
+    $scope.singin = function(){
+        //jsute sign in if every thing is OK
+    }
 
 });
